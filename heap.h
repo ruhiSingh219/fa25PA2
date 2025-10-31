42b68bc0e99b5684d4773b109eece0237a61acd4
Ruhi Singh <rsingh8416@sds.edu>
2025-10-31 01:28:10 -0700

    inital writing for push() and upheap()

--- heap.h
+++ heap.h
@@ -16,6 +16,15 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
+        if (size == 0) {
+            data[0] = 1;
+        } else {
+            data[size] = idx;
+            if (weightArr[data[size]] < weightArr[0]) {
+                upheap(data[size-1], weightArr);
+            }
+        }
+        size++;
     }
 
     int pop(int weightArr[]) {
@@ -26,11 +35,16 @@ struct MinHeap {
 
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
+        pos = size-1;
+        int posd = data[pos];
+        int parent = (pos-1) / 2;
+        while (pos > 0 && data[pos] < data[parent]) {
+            data[pos] = data[parent];
+            data[parent] = posd;
+        }
     }
-
     void downheap(int pos, int weightArr[]) {
         // TODO: swap parent downward while larger than any child
     }
 };
-
 #endif
