17b7b457d83edff1ee521c843600a4a86dc7be56
Ruhi Singh <rsingh8416@sds.edu>
2025-10-31 23:25:05 -0700

    first completion of heap.h

--- heap.h
+++ heap.h
@@ -29,22 +29,39 @@ struct MinHeap {
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        // Replace root with last element
+        int tmp = weightArr[data[size-1]];
+        size--;
+        weightArr[0] = tmp;
+        downheap(data[0], weightArr);
+        return 0;
     }
 
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
-        pos = size-1;
-        int posd = data[pos];
-        int parent = (pos-1) / 2;
-        while (pos > 0 && data[pos] < data[parent]) {
-            data[pos] = data[parent];
-            data[parent] = posd;
+        int parent = (pos - 1) / 2;
+        int tmp = data[pos];
+        while (weightArr[data[pos]] < weightArr[parent]) {
+            weightArr[data[pos]] = weightArr[data[parent]];
+            weightArr[data[parent]] = weightArr[tmp];
+            pos = pos/2;
         }
     }
     void downheap(int pos, int weightArr[]) {
         // TODO: swap parent downward while larger than any child
+        int lc = data[(2*pos) + 1];
+        int rc = data[(2*pos) + 2];
+        int tmp = data[pos];
+        while (weightArr[data[pos]] > weightArr[lc] || weightArr[data[pos]] > weightArr[rc]) {
+            if (weightArr[lc] < weightArr[rc]) {
+                weightArr[pos] = weightArr[lc];
+                weightArr[lc] = weightArr[tmp];
+            } else {
+                weightArr[pos] = weightArr[rc];
+                weightArr[rc] = weightArr[tmp];
+            }
+            pos = pos*2;
+        }
     }
 };
 #endif
