f72e1f08f2ee72f7a2a5eb1aa6991bfd8c1cee6e
Ruhi Singh <rsingh8416@sds.edu>
2025-11-01 00:03:20 -0700

    fix logic: heap.h

--- heap.h
+++ heap.h
@@ -6,6 +6,7 @@
 #define HEAP_H
 
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
 struct MinHeap {
@@ -16,52 +17,73 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         // TODO: insert index at end of heap, restore order using upheap()
-        if (size == 0) {
-            data[0] = 1;
-        } else {
-            data[size] = idx;
-            if (weightArr[data[size]] < weightArr[0]) {
-                upheap(data[size-1], weightArr);
-            }
+        //throw error if push onto full
+        if (size >= 64) {
+            throw runtime_error("Overflow");
         }
+        //add to end
+        data[size] = idx;
+        upheap(size, weightArr);
         size++;
     }
 
     int pop(int weightArr[]) {
         // TODO: remove and return smallest index
-        // Replace root with last element
-        int tmp = weightArr[data[size-1]];
+        // throw error if pop from empty.
+        if (size == 0) {
+            throw runtime_error("Underflow");
+        }
+        int minInx = data[0];
+        //move last to root
+        data[0] = data[size-1];
         size--;
-        weightArr[0] = tmp;
-        downheap(data[0], weightArr);
-        return 0;
+        if (size > 0) {
+            downheap(0, weightArr);
+        }
+        //return popped val
+        return minInx;
     }
 
     void upheap(int pos, int weightArr[]) {
-        // TODO: swap child upward while smaller than parent
-        int parent = (pos - 1) / 2;
-        int tmp = data[pos];
-        while (weightArr[data[pos]] < weightArr[parent]) {
-            weightArr[data[pos]] = weightArr[data[parent]];
-            weightArr[data[parent]] = weightArr[tmp];
-            pos = pos/2;
+        while (pos > 0) {
+            //parent index
+            int parent = (pos-1) / 2;
+            if (weightArr[data[pos]] < weightArr[data[parent]]) {
+                int tmp = data[pos];
+                data[pos] = data[parent];
+                data[parent] = tmp;
+                pos = parent;
+            } else {
+                //break if parent weight is less than child's
+                break;
+            }
         }
     }
     void downheap(int pos, int weightArr[]) {
         // TODO: swap parent downward while larger than any child
-        int lc = data[(2*pos) + 1];
-        int rc = data[(2*pos) + 2];
-        int tmp = data[pos];
-        while (weightArr[data[pos]] > weightArr[lc] || weightArr[data[pos]] > weightArr[rc]) {
-            if (weightArr[lc] < weightArr[rc]) {
-                weightArr[pos] = weightArr[lc];
-                weightArr[lc] = weightArr[tmp];
-            } else {
-                weightArr[pos] = weightArr[rc];
-                weightArr[rc] = weightArr[tmp];
-            }
-            pos = pos*2;
-        }
+
+         while (true) {
+             int lc = data[(2*pos) + 1];
+             int rc = data[(2*pos) + 2];
+             //break if no children
+             if (lc >= size) {
+                 break;
+             }
+
+             int smallest = lc;
+             if (rc < size && weightArr[data[rc]] < weightArr[data[lc]]) {
+                 smallest = rc;
+             }
+             //break if parent is already less than smallest child
+             if (weightArr[data[pos]] <= weightArr[data[smallest]]) {
+                 break;
+             }
+             int t = data[pos];
+             data[pos] = data[smallest];
+             data[smallest] = t;
+             //continue from child's postion
+             pos = smallest;
+         }
     }
 };
 #endif
